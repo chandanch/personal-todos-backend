4ba50ec89713ca074b91e8c7df50ed7ed706b206
chandanch <chandan.cec107@gmail.com>
2024-06-25 08:48:46 +0530

    use of dotenv to acces env vars :tada:

--- app.ts
+++ app.ts
@@ -1,10 +1,14 @@
 import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+// init dotenv
+dotenv.config();
 
 // 1. Create an instance of express
 const app: Express = express();
 
 // 2. Define the port on which the express app will run
-const APP_PORT: number = 4000;
+const APP_PORT: number = Number(process.env.APP_PORT) || 4000;
 
 // 3. Create an default route
 app.get('/', (req: Request, res: Response) => {
