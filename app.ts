fda4d5070a0e7832ddc5833dcb9f323f7f04729b
chandanch <chandan.cec107@gmail.com>
2024-06-26 23:21:54 +0530

    setup and integrate with typeorm :bulb:

--- app.ts
+++ app.ts
@@ -1,5 +1,6 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import { DataSource } from 'typeorm';
 
 // init dotenv
 dotenv.config();
@@ -10,12 +11,30 @@ const app: Express = express();
 // 2. Define the port on which the express app will run
 const APP_PORT: number = Number(process.env.APP_PORT) || 4000;
 
-// 3. Create an default route
+// 3. Create a connection to DB using DataSource
+const AppDataSource: DataSource = new DataSource({
+	type: 'mysql',
+	host: process.env.DB_HOST,
+	port: Number(process.env.DB_PORT),
+	username: process.env.DB_USER,
+	password: process.env.DB_PWD,
+	database: process.env.DB_NAME,
+	synchronize: true,
+	ssl: true,
+});
+
+// 4. Create an default route
 app.get('/', (req: Request, res: Response) => {
 	res.send('Server is running');
 });
 
-// 4. Start listening for requests on the above defined port
-app.listen(APP_PORT, '', () => {
-	console.log('Server started');
-});
+// 5. Init DB
+AppDataSource.initialize()
+	.then(() => {
+		console.log('DB Connection Successful');
+		// 4. Start listening for requests on the above defined port
+		app.listen(APP_PORT, '', () => {
+			console.log('Server started');
+		});
+	})
+	.catch((err) => console.log('Error when connecting to DB', err));
