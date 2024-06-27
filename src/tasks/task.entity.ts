20f8b03f45d25d85d0b794bbbdc25b31912f0bb4
chandanch <chandan.cec107@gmail.com>
2024-06-27 22:22:45 +0530

    add fields to task entity

--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -1,13 +1,32 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Priority } from '../enums/priority';
+import { Status } from '../enums/status';
 
 @Entity()
 export class Task {
 	@PrimaryGeneratedColumn('uuid')
 	id: number;
 
-	@Column('varchar')
+	@Column({ type: 'text' })
 	title: string;
 
-	@Column('varchar')
+	@Column({ type: 'longtext' })
 	description: string;
+
+	@Column({ type: 'varchar', length: 255 })
+	duedate: string;
+
+	@Column({
+		type: 'enum',
+		enum: Priority,
+		default: Priority.normal,
+	})
+	priority: Priority;
+
+	@Column({
+		type: 'enum',
+		enum: Status,
+		default: Status.todo,
+	})
+	status: Status;
 }
