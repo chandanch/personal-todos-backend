ae83634430e74bdf7e02cd483569ee4f9d3b74bc
chandanch <chandan.cec107@gmail.com>
2024-06-27 08:14:32 +0530

    add props to task entites :bulb:

--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -1,4 +1,13 @@
-import { Entity } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
-export class Task {}
+export class Task {
+	@PrimaryGeneratedColumn('uuid')
+	id: number;
+
+	@Column('varchar')
+	title: string;
+
+	@Column('varchar')
+	description: string;
+}
