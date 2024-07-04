0dacd1f39b027f7534481e202c7c789ce01f3f4b
chandanch <chandan.cec107@gmail.com>
2024-07-04 09:48:54 +0530

    error handling for get tasks :bulb:

--- src/tasks/task.router.ts
+++ src/tasks/task.router.ts
@@ -7,8 +7,12 @@ const taskRouter: Router = Router();
 
 taskRouter.get('/', async (req: Request, res: Response) => {
 	const taskController: TaskController = new TaskController();
-	const tasks = await taskController.fetchAllTasks();
-	res.status(200).send(tasks);
+	try {
+		const tasks = await taskController.fetchAllTasks();
+		res.status(200).send(tasks);
+	} catch (error) {
+		res.status(500).send({ error: 'An Error Occured' });
+	}
 });
 
 export { taskRouter, taskRouteBasePath };
