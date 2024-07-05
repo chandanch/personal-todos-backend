8d009547bcccb4dcfc3d48d95b27e6c027d2ceaa
chandanch <chandan.cec107@gmail.com>
2024-07-05 20:23:54 +0530

    revamp task controller and router

--- src/tasks/task.controller.ts
+++ src/tasks/task.controller.ts
@@ -1,9 +1,10 @@
+import { Request, Response } from 'express';
 import { AppDataSource } from '../../data-source';
 import { Task } from './task.entity';
 import { instanceToPlain } from 'class-transformer';
 
 class TaskController {
-	async fetchAllTasks(): Promise<Task[]> {
+	async fetchAllTasks(req: Request, res: Response): Promise<Response> {
 		const taskRepository = AppDataSource.getRepository(Task);
 
 		let tasks: Task[];
@@ -15,10 +16,11 @@ class TaskController {
 				},
 			});
 
-			return instanceToPlain(tasks) as Task[];
+			tasks = instanceToPlain(tasks) as Task[];
+
+			return res.status(200).json(tasks);
 		} catch (error) {
-			console.log(error);
-			throw error;
+			return res.status(500).send({ error: 'An Error has occurred' });
 		}
 	}
 }
--- src/tasks/task.router.ts
+++ src/tasks/task.router.ts
@@ -7,14 +7,7 @@ const taskRouteBasePath: string = '/tasks';
 
 const taskRouter: Router = Router();
 
-taskRouter.get('/', async (req: Request, res: Response) => {
-	try {
-		const tasks = await taskController.fetchAllTasks();
-		res.status(200).send(tasks);
-	} catch (error) {
-		res.status(500).send({ error: 'An Error Occured' });
-	}
-});
+taskRouter.get('/', taskController.fetchAllTasks);
 
 taskRouter.post(
 	'/',
